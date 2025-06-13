7a3ba9e995db8360b8a9b67ec5f25a3732495b93
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-13 15:49:32 +0200

    add snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,26 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+//solo console.log
+console.log(names)
+
+
+//ciclo for
+  for (let i =0 ; i < names.length;i++) {
+  const namesciclofor = names[i]
+  console.log(namesciclofor)
+} 
+
+
+//metodo forEach
+names.forEach((name) => {
+  console.log(name);
+});
+
+
+
+//metodo map
+const namesMap = names.map((name) => {
+  console.log(name);
+  return name;
+});
