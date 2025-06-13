3f5e21d7c7ef3a5020bd88c534fb99b51a1425a8
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-13 16:12:58 +0200

    add snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,22 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] + 1;
+}
+console.log(numbers);
+
+
+// map
+
+const numberUp = numbers.map((numbers) => {
+    return numbers;
+});
+console.log(numberUp);
+
+//for each
+
+numbers.forEach((numbers)=>{
+    console.log(numbers)
+})
